d3fcf78a7ae1d46f9176ac698829d5d79a5fa8b0
nothasm <toms.matos@gmail.com>
2020-04-02 14:50:01 -0300

    feat: add docker-compose, make cron independent

--- src/crons/createOrders.ts
+++ src/crons/createOrders.ts
@@ -1,5 +1,7 @@
+import "../db";
 import OrderService from "../services/Order.service";
 import PipedriveService from "../services/Pipedrive.service";
+import moment from "moment";
 
 const TEN_MIN = 10 * 60 * 1000;
 
@@ -7,29 +9,34 @@ function sleep(time: number) {
   return new Promise(resolve => setTimeout(resolve, time));
 }
 
-// idealy this would run in a independent worker
-// as the api is small I thought it would be better
-// to call it along with the server
-export async function syncOrders(logs: boolean = false) {
+const log = (message?: any, ...optionalParams: any[]) => console.log(`[${moment().format("LTS")}]`, message, ...optionalParams);
+
+export async function syncOrders() {
     try {
+        log("SYNCING ORDERS")
         for (;;) {
             try {
                 // here we get all deals with the won status
                 const deals = await PipedriveService.getWonDeals();
+
                 // loop throudh all the deals and createOrder
                 // as the Bling Api already validate if the order is
                 // already exists, there was no need for this validation
                 // on our side
                 deals.forEach(deal => OrderService.createOrder(deal)
-                    .catch(e => !logs || console.log(e)));
+                    .catch(error => log("ERROR: ", error.message)));
                 
             } catch (error) {
-                !logs || console.error("error :", error);
+                log("ERROR: ", error.message);
+                log("RETRYING IN 10 MINUTES");
             }
             await sleep(TEN_MIN);
         }
     } catch (error) {
-        !logs || console.error("error :", error);
+        console.error(error);
+        log("RETRYING IN 10 MINUTES")
         await sleep(TEN_MIN);
     }
 };
+
+syncOrders();
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
 import { app } from "./app";
 import "./db";
-import { syncOrders } from "./crons/createOrders";
 
 const port = process.env.PORT;
-
-if (process.env.AUTO_SYNC) syncOrders();
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`);
