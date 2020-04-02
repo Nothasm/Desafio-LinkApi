4b2adf2df8f275afd3aaabcfc480598cf7b68dbe
nothasm <toms.matos@gmail.com>
2020-04-02 15:34:17 -0300

    fix: errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    if (err.statusCode)
+    if (err.statusCode && err.toJSON)
         return res.status(err.statusCode).send({ error: err.toJSON() });
 
     res.status(500).send({
