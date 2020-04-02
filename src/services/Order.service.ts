e90e68b91d8a14bbb3fc165a7b19a3dd498668a6
nothasm <toms.matos@gmail.com>
2020-04-02 15:27:29 -0300

    chore: remove unnecessart try catch

--- src/services/Order.service.ts
+++ src/services/Order.service.ts
@@ -6,33 +6,30 @@ import blingService from "./Bling.service"
 class OrderService {
 
     async createOrder(deal: UpdateDeal) {
-        try {
-            // check wether the deal is won
-            if (!deal || !deal.status || deal.status !== DealStatus.WON)
-                throw new HttpError(400, "Wrong body");
-            
-            // create request on bling api
-            // if the request already exists it will throw an error
-            await blingService.createRequest({
-                clientName: deal.person_name,
-                code: deal.id,
-                description: deal.title,
-                value: deal.value
-            });
 
-            // if the request on bling is sucessful it should create
-            // an order on the database
-            await new Order({
-                name: deal.title,
-                clientName: deal.person_name,
-                date: deal.update_time,
-                value: deal.value,
-                currency: deal.currency
-            }).save();
+        // check wether the deal is won
+        if (!deal || !deal.status || deal.status !== DealStatus.WON)
+            throw new HttpError(400, "Wrong body");
+        
+        // create request on bling api
+        // if the request already exists it will throw an error
+        await blingService.createRequest({
+            clientName: deal.person_name,
+            code: deal.id,
+            description: deal.title,
+            value: deal.value
+        });
 
-        } catch (e) {
-            throw e;
-        }
+        // if the request on bling is sucessful it should create
+        // an order on the database
+        await new Order({
+            name: deal.title,
+            clientName: deal.person_name,
+            date: deal.update_time,
+            value: deal.value,
+            currency: deal.currency
+        }).save();
+
     }
 
     async getOrders() {
